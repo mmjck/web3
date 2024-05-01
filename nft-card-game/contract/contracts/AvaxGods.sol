94cafc3acc5482d1f3c732c7f084f3bb8b25d2d8
mmjck <msantosjck@gmail.com>
2024-04-30 21:45:08 -0400

    update contract

--- nft-card-game/contract/contracts/AvaxGods.sol
+++ nft-card-game/contract/contracts/AvaxGods.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
 contract AVAXGods is ERC1155, Ownable, ERC1155Supply {
@@ -138,14 +138,95 @@ contract AVAXGods is ERC1155, Ownable, ERC1155Supply {
     );
     event RoundEnded(address[2] damagedPlayers);
 
-    
     function setURI(string memory _uri) public onlyOwner {
-          _setURI(_uri);
+        _setURI(_uri);
     }
 
     constructor(string memory _metadataURI) ERC1155(_metadataURI) {
         baseURI = _metadataURI;
-          initialize();
+        initialize();
+    }
 
+    function initialize() private {
+        gameTokens.push(GameToken("", 0, 0, 0));
+        players.push(Player(address(0), "", 0, 0, false));
+        battles.push(
+            Battle(
+                BattleStatus.PENDING,
+                bytes32(0),
+                "",
+                [address(0), address(0)],
+                [0, 0],
+                address(0)
+            )
+        );
+    }
+
+    function registerPlayer(
+        string memory _name,
+        string memory _gameTokenName
+    ) external {
+        require(!isPlayer(msg.sender), "Player already registered");
+
+        uint256 _id = players.length;
+        players.push(Player(msg.sender, _name, 10, 25, false));
+        playerInfo[msg.sender] = _id;
+
+        createRandomGameToken(_gameTokenName);
+
+        emit NewPlayer(msg.sender, _name);
+    }
+
+    function createRandomGameToken(string memory _name) public {
+        require(!getPlayer(msg.sender).inBattle, "Player is in a battle");
+        require(isPlayer(msg.sender), "Please Register Player First");
+
+        _createGameToken(_name);
+    }
+
+    function _createGameToken(
+        string memory _name
+    ) internal returns (GameToken memory) {
+        uint256 randAttackStrength = _createRandomNum(
+            MAX_ATTACK_DEFEND_STRENGTH,
+            msg.sender
+        );
+        uint256 randDefenseStrength = MAX_ATTACK_DEFEND_STRENGTH -
+            randAttackStrength;
+
+        uint8 randId = uint8(
+            uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender))) %
+                100
+        );
+        randId = randId % 6;
+
+        if (randId == 0) {
+            randId++;
+        }
+
+        GameToken memory newGameToken = GameToken(
+            _name,
+            randId,
+            randAttackStrength,
+            randDefenseStrength
+        );
+    }
+
+    function _createRandomNum(
+        uint256 _max,
+        address _sender
+    ) internal view returns (uint256 randomValue) {
+        uint256 randomNum = uint256(
+            keccak256(
+                abi.encodePacked(block.difficulty, block.timestamp, _sender)
+            )
+        );
+
+        randomValue = randomNum % _max;
+        if (randomValue == 0) {
+            randomValue = _max / 2;
+        }
+
+        return randomValue;
     }
 }
