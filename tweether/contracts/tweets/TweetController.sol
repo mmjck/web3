5ffb3a0df694d7fb8b6251d16353641148d06a58
mmjck <msantosjck@gmail.com>
2024-06-21 19:34:05 -0400

    tweet controller

---
+++ tweether/contracts/tweets/TweetController.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "../helpers/BaseController.sol";
+import "../ContractManager.sol";
+import "./TweetStorage.sol";
+
+contract TweetController is BaseController {
+    function createTweet(
+        uint _userId,
+        string memory _text
+    ) public returns (uint) {
+        ContractManager _manager = ContractManager(managerAddr);
+
+        address _tweetStorageAddr = _manager.getAddress("TweetStorage");
+        TweetStorage _tweetStorage = TweetStorage(_tweetStorageAddr);
+
+        return _tweetStorage.createTweet(_userId, _text);
+    }
+}
